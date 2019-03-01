ffa5ebb459cfab9ef58141cda8fb814dd52dde17
Stefanos Orovas <stef.orovas@gmail.com>
2019-03-01 02:52:43 +0200

    updated map tiles

--- code/views/map/index.jsx
+++ code/views/map/index.jsx
@@ -1,21 +1,25 @@
 import { graphql, compose } from 'react-apollo';
 import { Link } from 'react-router-dom';
-// import * as L from 'leaflet';
+import * as L from 'leaflet';
 import React, { Component } from 'react';
-import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import MapBoxGLLayer from '../../mapbox';
+import { Map as LeafletMap, Marker, Popup } from 'react-leaflet';
 import { Card } from 'antd';
 import { FETCH_POIS } from 'gql/poi';
-// import pin from '../../assets/pin.png';
+import pin from '../../assets/pin.png';
 import './style.less';
 
-// const pinIcon = L.icon({
-//   iconUrl: pin,
-//   iconSize: [38, 55], // size of the icon
-//   shadowSize: [50, 64], // size of the shadow
-//   iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-//   shadowAnchor: [4, 62], // the same for the shadow
-//   popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-// });
+const MAPBOX_ACCESS_TOKEN =
+  'pk.eyJ1Ijoic3RlZm9yb3ZhcyIsImEiOiJjaXd0aWVtajYwMDEzMm9reTk5c2ZwZm11In0.MUR4ozZ5d1I_oHE3APDjrw';
+
+const pinIcon = L.icon({
+  iconUrl: pin,
+  iconSize: [32, 32], // size of the icon
+  shadowSize: [100, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 class Map extends Component {
   static propTypes = {
@@ -38,14 +42,14 @@ class Map extends Component {
         zoom={this.state.zoom}
         zoomControl={false}
       >
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        <MapBoxGLLayer
+          accessToken={MAPBOX_ACCESS_TOKEN}
+          style='mapbox://styles/steforovas/cjspbgcyb1uv71fmstjk89zop'
         />
         {fetchPois.pois &&
           !fetchPois.loading &&
           fetchPois.pois.map(poi => (
-            <Marker position={poi.location.coordinates}>
+            <Marker icon={pinIcon} position={poi.location.coordinates}>
               <Popup className='map__popup'>
                 <Card
                   className='map__popup-card'
