1f85df7a0c0253512e1314f827221beb1ac016ac
Stefanos Orovas <stef.orovas@gmail.com>
2019-03-13 04:01:09 +0200

    added protected route

--- code/index.js
+++ code/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, Query } from 'react-apollo';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import {} from 'react-apollo';
+import { ME } from 'gql/user';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import client from './client';
-import Signup from './views/signup/signup';
+import Signin from './views/signin';
 import Map from './views/map';
 import PoiView from './views/pois/view';
 import PoiCreate from './views/pois/create';
@@ -17,20 +19,57 @@ import Contribute from 'views/contribute';
 
 import 'regenerator-runtime/runtime';
 
+class PrivateRoute extends React.Component {
+  static propTypes = {
+    Component: PropTypes.any,
+    location: PropTypes.object,
+  };
+
+  render() {
+    const { Component, location } = this.props;
+    return (
+      <Query query={ME}>
+        {({ loading, error, data }) => {
+          if (loading) {
+            return 'loading...';
+          }
+          return (
+            <Route
+              {...this.props}
+              render={props =>
+                (typeof error !== 'undefined' ? (
+                  <Redirect
+                    to={{
+                      pathname: '/signin',
+                      state: { referrer: location.pathname }
+                    }}
+                  />
+                ) : (
+                  <Component me={data.me} {...props} />
+                ))
+              }
+            />
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
 ReactDOM.render(
   <ApolloProvider client={client}>
     <BrowserRouter>
       <App>
         <Switch>
           <Route path='/' exact component={Map} />
-          <Route path='/signup' exact component={Signup} />
+          <Route path='/signin' exact component={Signin} />
           <Route path='/contribute' exact component={Contribute} />
           <Route path='/my-pois' exact component={Map} />
-          <Route path='/poi/create' exact component={PoiCreate} />
+          <PrivateRoute path='/poi/create' exact Component={PoiCreate} />
           <Route path='/poi/:id' exact component={PoiView} />
           <Route path='/poi/:id/edit' exact component={PoiEdit} />
           <Route path='/feedback' exact component={Feedback} />
-          <Route path='/account-info' exact component={AccountInfo} />
+          <PrivateRoute path='/account-info' exact Component={AccountInfo} />
         </Switch>
       </App>
     </BrowserRouter>
