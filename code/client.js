0c1b8affd006269a6550c57f4dae94fc18e94657
John Raptis <johnjohn_raptis@yahoo.gr>
2019-06-08 22:16:58 +0300

    update

--- code/client.js
+++ code/client.js
@@ -5,8 +5,8 @@ import { HttpLink } from 'apollo-link-http';
 
 const httpLink = new HttpLink({
   uri: __DEVELOPMENT__
-    ? 'https://api.locus.dimos.io/v1alpha1/graphql'
-    : 'https://api.locus.dimos.io/v1alpha1/graphql'
+  ? 'http://localhost:8080/v1alpha1/graphql'
+  : 'https://api.locus.dimos.io/v1alpha1/graphql'
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -14,10 +14,10 @@ const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('token');
   // return the headers to the context so httpLink can read them
   const authHeaders = token
-    ? {
-        Authorization: `Bearer ${token}`
-      }
-    : {};
+  ? {
+    Authorization: `Bearer ${token}`
+  }
+  : {};
   return {
     headers: Object.assign({}, headers, authHeaders)
   };
